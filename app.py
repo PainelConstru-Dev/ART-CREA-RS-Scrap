f73cced7d7d200d4f45d24ed137c987139fe626d
mundstock <igordaudt@gmail.com>
2025-02-28 15:04:02 -0300

    adding things

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from ARTsScrap import *
 
 def main():
-    start_scrap(12800000, 1)
+    start_scrap(13700202, 1)
 
 if __name__ == '__main__':
     main()
--- app4.py
+++
@@ -1,7 +0,0 @@
-from ARTsScrap import *
-
-def main():
-    start_scrap(13100000, 4)
-
-if __name__ == '__main__':
-    main()
